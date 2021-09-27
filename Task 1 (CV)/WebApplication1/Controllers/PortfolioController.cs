17ea0e27a2b3a201d1108a11474acca71cf783c8
talha-51 <mahbuburrahmantalha@gmail.com>
2021-09-27 13:07:51 +0600

    Demo CV with .net

---
+++ Task 1 (CV)/WebApplication1/Controllers/PortfolioController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public ActionResult Bio()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
